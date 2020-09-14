36414a5f1bed6a8424fca26ff190cea503da8b81
valnuccio <val.nuccio@gmail.com>
2020-09-14 16:47:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    modified();
+  })
+
+
+function modified() {
+    let area = document.getElementById("text");
+    area.textContent = "This is really cool!";
+
+
+}
+
+
+
 // Your code goes here
