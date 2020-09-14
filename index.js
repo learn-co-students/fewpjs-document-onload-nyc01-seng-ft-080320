2c7736746e105de900085af0afffd1babf08405d
soroosh-gb <soroosh.gb@gmail.com>
2020-09-14 18:03:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let id = document.getElementById('text')
+    id.textContent = "This is really cool!"
+
+});
+
