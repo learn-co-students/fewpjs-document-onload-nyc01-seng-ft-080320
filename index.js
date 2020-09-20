d9ea61f061723dce1e686770d98fe9431c9e4bf4
dkim61 <dkim187@binghamton.edu>
2020-09-20 01:07:07 -0400

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ document.addEventListener( "DOMContentLoaded", function () {
   
   
   function updateDOM() {
-    document.getElementById( "text" )
-      .innerHTML = "This is really cool!";
+    document.getElementById("text").innerHTML = "This is really cool!";
   }
